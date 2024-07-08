c858f7c0b008dba8d29fc9aa5f5b32efa7778d2d
Linda Boughattas <linda.boughattas@enicar.ucar.tn>
2024-07-08 20:55:01 +0200

    adding firebase to my project and updating ul

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://playground-a5a93-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    inputFieldEl.value = ""
+    push(shoppingListInDB, inputValue)
+    shoppingListEl.innerHTML += `<li>${inputValue}</li>`
+    console.log(inputValue)
+})
